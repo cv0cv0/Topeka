0ec1f82a0062706c92fe9917250c1851af08bb2a
cv0cv0 <linux2.0@foxmail.com>
2018-10-20 16:34:46 +0800

    Initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ buildscript {
         aliyun()
     }
     dependencies {
-        classpath(buildGradle)
-        classpath(kotlinGradle)
+        classpath(buildTools)
+        classpath(kotlinPlugin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files

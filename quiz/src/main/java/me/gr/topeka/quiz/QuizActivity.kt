25165d0df937b776b87bb5a1bf645e49feeb92c9
cv0cv0 <linux2.0@foxmail.com>
2018-11-20 14:00:21 +0800

    Initial commit

--- quiz/src/main/java/me/gr/topeka/quiz/QuizActivity.kt
+++ quiz/src/main/java/me/gr/topeka/quiz/QuizActivity.kt
@@ -2,16 +2,21 @@ package me.gr.topeka.quiz
 
 import android.animation.Animator
 import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.interpolator.view.animation.FastOutSlowInInterpolator
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.instantapps.InstantApps
+import kotlinx.android.synthetic.main.activity_quiz.*
 import me.gr.topeka.base.data.Category
+import me.gr.topeka.base.extension.db
+import me.gr.topeka.base.helper.isLogged
 import me.gr.topeka.base.helper.launchCategory
+import me.gr.topeka.base.helper.requestLogin
 import me.gr.topeka.quiz.transition.SharedTextCallback
 import me.gr.topeka.quiz.ui.QuizFragment
 import org.jetbrains.anko.AnkoLogger
@@ -23,10 +28,10 @@ class QuizActivity : AppCompatActivity(), AnkoLogger {
     private lateinit var revealAnimator: Animator
     private lateinit var colorAnimator: ObjectAnimator
     private lateinit var category: Category
-    private val interpolator = FastOutSlowInInterpolator()
     private var isPlaying = false
 
     companion object {
+        private val interpolator = FastOutSlowInInterpolator()
         private const val CATEGORY_IMAGE_PREFIX = "image_category_"
         private const val IS_PLAYING = "is_playing"
         private const val QUIZ_FRAGMENT_TAG = "Quiz"
@@ -34,7 +39,6 @@ class QuizActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz)
         if (savedInstanceState != null) {
             isPlaying = savedInstanceState.getBoolean(IS_PLAYING)
         }
@@ -58,6 +62,8 @@ class QuizActivity : AppCompatActivity(), AnkoLogger {
                 sharedElementSnapshots: MutableList<View>?
             ) {
                 super.onSharedElementStart(sharedElementNames, sharedElements, sharedElementSnapshots)
+                back_button.scaleX = 0f
+                back_button.scaleY = 0f
             }
 
             override fun onSharedElementEnd(
@@ -66,11 +72,53 @@ class QuizActivity : AppCompatActivity(), AnkoLogger {
                 sharedElementSnapshots: MutableList<View>?
             ) {
                 super.onSharedElementEnd(sharedElementNames, sharedElements, sharedElementSnapshots)
+                ViewCompat.animate(back_button)
+                    .setStartDelay(startDelay)
+                    .scaleX(1f)
+                    .scaleY(1f)
+                    .alpha(1f)
             }
         })
+        if (!isLogged()) requestLogin { }
     }
 
     private fun populate(categoryId: String) {
+        category = db.getCategoryById(categoryId)
+        setTheme(category.theme.style)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, category.theme.primaryDarkColor)
+        }
+        initLayout()
+    }
+
+    private fun initLayout() {
+        setContentView(R.layout.activity_quiz)
+        val packageName = packageName +
+                if (InstantApps.isInstantApp(this)) ".quiz" else ""
+        val drawableRes = resources.getIdentifier(
+            "$CATEGORY_IMAGE_PREFIX${category.id}",
+            "drawable",
+            packageName
+        )
+        image_view.setImageResource(drawableRes)
+        ViewCompat.animate(image_view)
+            .scaleX(1f)
+            .scaleY(1f)
+            .alpha(1f)
+            .setInterpolator(interpolator)
+            .setStartDelay(300L)
+            .start()
+        with(play_floating) {
+            setImageResource(R_base.drawable.ic_play)
+            if (isPlaying) hide() else show()
+            setOnClickListener { startQuiz() }
+        }
+        back_button.setOnClickListener { onBackPressed() }
+        title_text.text = category.name
+        title_text.setTextColor(ContextCompat.getColor(this, category.theme.textPrimaryColor))
+    }
+
+    private fun startQuiz() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
